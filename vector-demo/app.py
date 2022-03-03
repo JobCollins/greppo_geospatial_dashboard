acb0a96ad212fb0c682d5f55da4ff474ab7e8fad
JobCollins <dulo.collins@gmail.com>
2022-03-03 12:58:35 +0300

    Feature: Data Display Import dataset and display as an overlay of the map.

--- vector-demo/app.py
+++ vector-demo/app.py
@@ -1,4 +1,22 @@
+import geopandas as gpd
 from greppo import app
+
+counties = gpd.read_file("./kenyan-counties.geojson")
+roads = gpd.read_file("/Users/jobdulo/Documents/geo_data/Kenya_roads.json")
+
+app.vector_layer(
+    data = counties,
+    name = "Counties of Kenya",
+    description = "Boundaries of the counties of Kenya",
+    style = {"fillColor":"#4daf4a"}
+)
+
+app.vector_layer(
+    data = roads,
+    name = "Main Roads and Highways in Kenya",
+    description = "Major roads and highways of Kenya",
+    style = {"fillColor":"#377eb8"}
+)
 
 app.base_layer(
     name="Open Stree Map",
